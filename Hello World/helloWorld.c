6f12da203b6bae2d55f62b45060430b192f801ae
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-18 19:51:55 +0200

    Added code for helloWorld.c helloWorld.c prints a message for each communicator in the environment

--- Hello World/helloWorld.c
+++ Hello World/helloWorld.c
@@ -1,3 +1,23 @@
 //
 // Created by matth on 2024/05/18.
 //
+# include <mpi.h>
+# include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int num_procs;
+    int rank;
+
+    //Set up environment
+    MPI_Init(&argc, &argv);
+    //Get communicator size (number of processors in environment
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    //Get rank of current communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf("Hello World from communcator %d of %d!\n", rank, num_procs);
+
+    //Close parallel environment
+    MPI_Finalize();
+}
