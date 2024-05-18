ad7da281378ed7fa987a6d3dcc141da98ab10c9d
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-18 19:56:53 +0200

    made comments more informative

--- Hello World/helloWorld.c
+++ Hello World/helloWorld.c
@@ -9,15 +9,15 @@ int main(int argc, char **argv)
     int num_procs;
     int rank;
 
-    //Set up environment
+    //Set up MPI environment
     MPI_Init(&argc, &argv);
     //Get communicator size (number of processors in environment
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     //Get rank of current communicator
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("Hello World from communcator %d of %d!\n", rank, num_procs);
+    printf("Hello World from communicator %d of %d!\n", rank, num_procs);
 
-    //Close parallel environment
+    //Close MPI environment
     MPI_Finalize();
 }
