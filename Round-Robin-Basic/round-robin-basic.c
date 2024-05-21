7612378f23a2b9e766bbafb1ca9064aa1c7aaab2
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-21 11:42:16 +0200

    sent output to output.txt and sorted output in sorted.txt

--- Round-Robin-Basic/round-robin-basic.c
+++ Round-Robin-Basic/round-robin-basic.c
@@ -3,8 +3,11 @@
 //
 # include <mpi.h>
 # include <stdio.h>
+# include <time.h>
+#include <stdlib.h>
+#include <limits.h>
 
-void round_robin(int rank, int procs) {
+void round_robin(int rank, int procs, FILE *fptr) {
            //this processors random value
     long int rand_mine, rand_prev; //random values
                       //random value received from previous processor
@@ -16,41 +19,46 @@ void round_robin(int rank, int procs) {
 
     srandom(time(NULL) + rank);//seed the random value
     rand_mine = random() / (RAND_MAX / 100);
-    printf("%d: random is %ld\n", rank, rand_mine);
+    fprintf(fptr,"%d: random is %ld\n", rank, rand_mine);
 
     if (rank%2==0){//if the processors are even, send first, then receive
-    printf("%d: sending %ld to %d\n", rank, rand_mine, rank_next);
+    fprintf(fptr,"%d: sending %ld to %d\n", rank, rand_mine, rank_next);
                                //number of longs to send //tag is 1
     MPI_Send((void *)&rand_mine, 1, MPI_LONG, rank_next, 1, MPI_COMM_WORLD);
             //buffer              //sending a long //destination is rank_next
-    printf("%d: receiving from %d\n", rank, rank_prev);
+    fprintf(fptr,"%d: receiving from %d\n", rank, rank_prev);
         //number of longs to send //tag is 1
     MPI_Recv((void *)&rand_prev, 1, MPI_LONG, rank_prev, 1, MPI_COMM_WORLD, &status);
         //buffer              //receiving a long //source is rank_prev     //status of communication
-    else {
-    printf("%d: receiving from %d\n", rank, rank_prev);
+    }else {
+    fprintf(fptr,"%d: receiving from %d\n", rank, rank_prev);
     MPI_Recv((void *)&rand_prev, 1, MPI_LONG, rank_prev, 1, MPI_COMM_WORLD, &status);
 
-    printf("%d: sending %ld to %d\n", rank, rand_mine, rank_next);
+    fprintf(fptr,"%d: sending %ld to %d\n", rank, rand_mine, rank_next);
     MPI_Send((void *)&rand_mine, 1, MPI_LONG, rank_next, 1, MPI_COMM_WORLD);
 
     }
 
+    fprintf(fptr,"%d: >> I had %ld, %d had %ld\n", rank, rand_mine, rank_prev, rand_prev);
 }
 int main(int argc, char **argv)
 {
     int num_procs;
     int rank;
+    FILE *fptr;
+    fptr = fopen("output.txt","w");
 
+    fptr = fopen("output.txt","a");
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("Hello World from communicator %d of %d!\n", rank, num_procs);
-    round_robin(rank, num_procs);
+    fprintf(fptr,"%d: Hello World from communicator %d of %d!\n", rank, rank, num_procs);
+    round_robin(rank, num_procs, fptr);
+
 
     MPI_Finalize();
 
-
+    fclose(fptr);
 }
 
