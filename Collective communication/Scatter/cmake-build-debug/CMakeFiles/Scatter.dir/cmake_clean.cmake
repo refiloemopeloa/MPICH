d7d7e2f3da609d0b518211e49297249f76eb0f76
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-31 13:15:10 +0200

    Organised programs into collective communication and point-to-point communication

---
+++ Collective communication/Scatter/cmake-build-debug/CMakeFiles/Scatter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scatter.dir/Scatter.c.o"
+  "CMakeFiles/Scatter.dir/Scatter.c.o.d"
+  "Scatter"
+  "Scatter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Scatter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Collective communication/Scatter/cmake-build-debug/CMakeFiles/Scatter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Scatter.
+# This may be replaced when dependencies are built.
