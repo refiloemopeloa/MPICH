0204cafde7a1686e2ae3ec98731b7527451ce3d2
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-31 11:42:27 +0200

    CMake files

---
+++ Reduction/cmake-build-debug/CMakeFiles/reduction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reduction.dir/reduction.c.o"
+  "CMakeFiles/reduction.dir/reduction.c.o.d"
+  "reduction"
+  "reduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
