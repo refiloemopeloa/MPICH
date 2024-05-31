73495b7c6a38b6b63eac4ed604b645169d350598
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-05-31 11:04:30 +0200

    compiled non-block-one-to-many.c using CMakeList

---
+++ Non-block-one-to-many/cmake-build-debug/CMakeFiles/non-block-one-to-many.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/non-block-one-to-many.dir/non-block-one-to-many.c.o"
+  "CMakeFiles/non-block-one-to-many.dir/non-block-one-to-many.c.o.d"
+  "non-block-one-to-many"
+  "non-block-one-to-many.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/non-block-one-to-many.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Non-block-one-to-many/cmake-build-debug/CMakeFiles/non-block-one-to-many.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for non-block-one-to-many.
+# This may be replaced when dependencies are built.
