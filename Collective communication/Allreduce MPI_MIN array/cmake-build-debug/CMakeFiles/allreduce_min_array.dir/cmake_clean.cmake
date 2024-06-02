2797cd74549131ccadbc31879841b969e407635d
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-06-02 23:51:03 +0200

    CMake files

---
+++ Collective communication/Allreduce MPI_MIN array/cmake-build-debug/CMakeFiles/allreduce_min_array.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/allreduce_min_array.dir/allreduce_min_array.c.o"
+  "CMakeFiles/allreduce_min_array.dir/allreduce_min_array.c.o.d"
+  "allreduce_min_array"
+  "allreduce_min_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/allreduce_min_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Collective communication/Allreduce MPI_MIN array/cmake-build-debug/CMakeFiles/allreduce_min_array.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for allreduce_min_array.
+# This may be replaced when dependencies are built.
